7ad556f4efdb22e49b64fec972b70fd731cea077
mnlpdr <pedro.academico@ifpb.edu.br>
2023-04-20 18:31:14 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Produto } from './produto';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-crud';
+  nome = '';
+  descricao = '';
+  preco: number = 0;
+  produtos: Produto[] = [];
+  idCounter = 1;
+
+  adicionarProduto() {
+    const novoProduto = new Produto(this.idCounter++, this.nome, this.descricao, this.preco);
+    this.produtos.push(novoProduto);
+    this.limparCampos();
+  }
+
+  editarProduto(produto: Produto) {
+    this.nome = produto.nome;
+    this.descricao = produto.descricao;
+    this.preco = produto.preco;
+  }
+
+  removerProduto(id: number) {
+    this.produtos = this.produtos.filter((produto) => produto.id !== id);
+  }
+
+  limparCampos() {
+    this.nome = '';
+    this.descricao = '';
+    this.preco = 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
